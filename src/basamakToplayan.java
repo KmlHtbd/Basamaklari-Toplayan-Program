cab083d55c929cabc02355c989ac0671bc9f71c0
kml htb <kmlhtbdev@gmail.com>
2022-10-16 16:22:40 +0300

    Initial commit

---
+++ src/basamakToplayan.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class basamakToplayan {
+    public static void main(String[] args) {
+
+        int a,result=0,stepValue;
+
+        Scanner input =new Scanner(System.in);
+        System.out.println("Bir sayı giriniz:");
+        a = input.nextInt();
+
+        while (a != 0){
+            stepValue = a%10;
+            result += stepValue;
+            System.out.println(stepValue);
+            a /= 10;
+        }
+    }
+}
